48cad29381ac39c308fab0a47d21bd8a9fef12bc
Makers Academy <onboarding@makersacademy.com>
2018-10-16 11:48:34 +0000

    friday challenge

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,6 +1,18 @@
 class Menu
-    
-    def check(dish)
-    end
-    
+  attr_reader :dishes 
+  
+  def initialize
+    @dishes = [
+     { dish: "fish and chips", price: 3},
+       {dish: "chicken and chips", price: 3},
+       {dish: "chips", price: 1},
+       {dish: "fries", price: 1.50},
+       {dish: "pie and chips", price: 3 },
+       ]
+  end
+  def list
+    @dishes.each_with_index { |dish, index|
+    puts "#{index+1} #{dish[:dish]}: £#{dish[:price]}"
+    }
+  end 
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,9 +1,18 @@
 require 'menu'
 
 describe Menu do
-    subject(:menu) { described_class.new }
-    it 'checks for menu prices' do
-      expect(menu).to respond_to(:price).with(1).argument
-    end
+  subject(:menu) { described_class.new }
 
+  it 'checks for menu prices' do
+    expect(menu).to respond_to(:price).with(1).argument
+  end
+    
+  describe '#list' do 
+    it 'responds to list' do 
+      expect(subject).to respond_to(:list)
+  end 
+  it 'prints the menu' do 
+    expect { subject.list }.to output("1. fish and chips: £3/n2. chicken and chips: £3/n3. chips: £1/n3. fries: £1.50/n4. pie and chips: £3/n").to_stdout
+ end 
 end 
+end
